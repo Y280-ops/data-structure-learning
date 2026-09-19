606b970ef8044cea2a7bc98c0e39cf3e94a75899
Y280-ops <2450498334@qq.com>
2026-09-19 23:30:28 +0800

    Rename FileName.c to linked_list_basic.c

--- 线性表Linear List/Linked List链表/linked_list_basic.c
+++ 线性表Linear List/Linked List链表/linked_list_basic.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-//������һ���ṹ����������ΪBOOK���൱��int��char�ȣ�
+//定义了一个结构体类型名称为BOOK（相当于int、char等）
 
 typedef int BOOK;
 
@@ -12,7 +12,7 @@ typedef struct node{
     struct node *next;
 }Node;
 
-//��ʼ����������ͷ���
+//初始化，带有了头结点
 
 Node* InitList()
 {
@@ -21,7 +21,7 @@ Node* InitList()
     return list;
 }
 
-//��ʼ����������ͷ���
+//初始化，不带有头结点
 
 Node* InitList1(BOOK book)
 {
@@ -31,7 +31,7 @@ Node* InitList1(BOOK book)
     return list;
 }
 
-//����
+//遍历
 
 void ListNode(Node* list)
 {
@@ -43,7 +43,7 @@ void ListNode(Node* list)
     }
 }
 
-//ͷ�巨�����ǻᷢ�������ͷ���Ļ�����Ҫ�����������浽����û�ж�����
+//头插法（我们会发现如果有头结点的话不需要考虑链表里面到底有没有东西）
 
 void ListInsertHead(Node* list, BOOK book)
 {
@@ -53,7 +53,7 @@ void ListInsertHead(Node* list, BOOK book)
     list->next = newNode;
 }
 
-//β�巨��list�����Ǹ�ͷ��㣬Ȼ����Ҫһ����ʱ�ڵ����ƣ�
+//尾插法（list还是那个头结点，然后需要一个临时节点来推）
 
 void ListInsertTail(Node* list, BOOK book)
 {
@@ -68,13 +68,13 @@ void ListInsertTail(Node* list, BOOK book)
     node->next = newNode;
 }
 
-//ָ��λ�ò���
+//指定位置插入
 
 void ListInsert(Node* list, int pos, BOOK book)
 {
     if (pos <= 0)
     {
-        printf("����λ�ô���");
+        printf("插入位置错误");
         return;
     }
     Node* newNode = (Node*)malloc(sizeof(Node));
@@ -85,7 +85,7 @@ void ListInsert(Node* list, int pos, BOOK book)
         node = node->next;
         if (node == NULL)
         {
-            printf("����λ�ô���");
+            printf("插入位置错误");
             return;
         }
     }
@@ -93,13 +93,13 @@ void ListInsert(Node* list, int pos, BOOK book)
     node->next = newNode;
 }
 
-//ָ��λ��ɾ��
+//指定位置删除
 
 void ListDelete(Node* list, int pos)
 {
     if (pos <= 0)
     {
-        printf("ɾ��λ�ô���");
+        printf("删除位置错误");
         return;
     }
     Node* node = list;
@@ -107,7 +107,7 @@ void ListDelete(Node* list, int pos)
     {
         if (node->next == NULL)
         {
-            printf("ɾ��λ�ô���");
+            printf("删除位置错误");
             return;
         }
         node = node->next;
